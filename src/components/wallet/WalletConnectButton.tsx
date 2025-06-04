a3f48b5a65c0d66e572c22ec7a4682ac1d30ecb2
R84N <148795329+R84N@users.noreply.github.com>
2025-06-04 21:00:09 +0300

    Update WalletConnectButton.tsx

--- src/components/wallet/WalletConnectButton.tsx
+++ src/components/wallet/WalletConnectButton.tsx
@@ -1,5 +1,5 @@
-import { useWallet } from '../../hooks/useWallet';
-import { Button } from '../ui/Button';
+
+
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 export function WalletConnectButton() {
